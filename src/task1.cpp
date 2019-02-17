265d215478018ed552070fcac23580a16b101b5c
melonehco <mzmao@colby.edu>
2019-02-17 11:39:44 -0500

    added task1.cpp

---
+++ src/task1.cpp
@@ -0,0 +1,18 @@
+/* task1.cpp
+ * Implements a content-based image retrieval (CBIR) program.
+ * Takes in a query image and an image database, matches the query image
+ * to each database image using a distance metric, then sorts the database
+ * images according to their similarity to the query images
+ * 
+ * to run:
+ * <path-to-bin>/bin/task1 <# results to show> <optional db directory>
+ * 
+ * Zena Abulhab and Melody Mao
+ * CS365 Spring 2019
+ * Project 2
+ */
+
+int main(int argc, char *argv[]) {
+    
+
+}
